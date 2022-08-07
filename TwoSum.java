141fabab035432daf6b17e2953135b11d11df1a1
Nick McCrea <57852809+Storm-07@users.noreply.github.com>
2022-08-07 11:37:48 -0400

    Create TwoSum.java

---
+++ TwoSum.java
@@ -0,0 +1,14 @@
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int[] twonums = new int[2];
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = 0; j < nums.length; j++) {
+                if((nums[i] + nums[j] == target) && (i != j)) {
+                    twonums[0] = i;
+                    twonums[1] = j;
+                }
+            }
+        }
+        return twonums;
+    }
+}
